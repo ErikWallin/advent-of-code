ac1abb771b2a3b9ef30d1a14fdaffe65963bb933
Erik Wallin <wallin_erik@yahoo.com>
2022-12-02 06:26:36 +0100

    2022/02

---
+++ 2022/02/input.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/ErikWallin/advent-of-code/common"
+)
+
+var tests = common.TestCases{
+	{
+		Input: `A Y
+B X
+C Z`,
+		ExpectedPart1: 15,
+		ExpectedPart2: 12,
+	},
+}
---
+++ 2022/02/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"strings"
+
+	"github.com/ErikWallin/advent-of-code/common"
+)
+
+func run1(input string) int {
+	rows := common.ParseStringList(input, "\n")
+	score := 0
+	for _, row := range rows {
+		shapes := strings.Split(row, " ")
+		if shapes[0] == "A" {
+			if shapes[1] == "X" {
+				score += 3
+				score += 1
+			} else if shapes[1] == "Y" {
+				score += 6
+				score += 2
+			} else if shapes[1] == "Z" {
+				score += 0
+				score += 3
+			}
+		} else if shapes[0] == "B" {
+			if shapes[1] == "X" {
+				score += 0
+				score += 1
+			} else if shapes[1] == "Y" {
+				score += 3
+				score += 2
+			} else if shapes[1] == "Z" {
+				score += 6
+				score += 3
+			}
+		} else if shapes[0] == "C" {
+			if shapes[1] == "X" {
+				score += 6
+				score += 1
+			} else if shapes[1] == "Y" {
+				score += 0
+				score += 2
+			} else if shapes[1] == "Z" {
+				score += 3
+				score += 3
+			}
+		}
+	}
+	return score
+}
+
+func run2(input string) int {
+	rows := common.ParseStringList(input, "\n")
+	score := 0
+	for _, row := range rows {
+		shapes := strings.Split(row, " ")
+		if shapes[0] == "A" {
+			if shapes[1] == "X" {
+				score += 3
+				score += 0
+			} else if shapes[1] == "Y" {
+				score += 1
+				score += 3
+			} else if shapes[1] == "Z" {
+				score += 2
+				score += 6
+			}
+		} else if shapes[0] == "B" {
+			if shapes[1] == "X" {
+				score += 1
+				score += 0
+			} else if shapes[1] == "Y" {
+				score += 2
+				score += 3
+			} else if shapes[1] == "Z" {
+				score += 3
+				score += 6
+			}
+		} else if shapes[0] == "C" {
+			if shapes[1] == "X" {
+				score += 2
+				score += 0
+			} else if shapes[1] == "Y" {
+				score += 3
+				score += 3
+			} else if shapes[1] == "Z" {
+				score += 1
+				score += 6
+			}
+		}
+	}
+	return score
+}
+
+func main() {
+	submit := true
+	verbose := false
+	common.Run(run1, run2, tests, 2022, 2, submit, verbose)
+}
