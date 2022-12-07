15856c0610ffba354a4b34550688e1578db373ad
Erik Wallin <wallin_erik@yahoo.com>
2022-12-07 07:05:28 +0100

    2022/07

--- 2022/07/input.go
+++ 2022/07/input.go
@@ -5,9 +5,31 @@ import (
 )
 
 var tests = common.TestCases{
-	/*{
-		Input:         ``,
-		ExpectedPart1: 0,
-		ExpectedPart2: 0,
-	},*/
+	{
+		Input: `$ cd /
+$ ls
+dir a
+14848514 b.txt
+8504156 c.dat
+dir d
+$ cd a
+$ ls
+dir e
+29116 f
+2557 g
+62596 h.lst
+$ cd e
+$ ls
+584 i
+$ cd ..
+$ cd ..
+$ cd d
+$ ls
+4060174 j
+8033020 d.log
+5626152 d.ext
+7214296 k`,
+		ExpectedPart1: 95437,
+		ExpectedPart2: 24933642,
+	},
 }
--- 2022/07/main.go
+++ 2022/07/main.go
@@ -1,21 +1,101 @@
 package main
 
 import (
+	"strings"
+
 	"github.com/ErikWallin/advent-of-code/common"
 )
 
 func run1(input string) interface{} {
-	//list := common.ParseIntList(input, "\n")
-	return 0
+	list := common.ParseStringList(input, "\n")[1:]
+	var stack []string
+	dirs := map[string]int{}
+	shouldAdd := true
+	for _, row := range list {
+		if row == "$ cd .." {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		args := strings.Split(row, " ")
+		if args[0] == "dir" || args[1] == "ls" {
+			continue
+		}
+		if args[1] == "cd" {
+			stack = append(stack, args[2])
+			dirs[path(stack)] = 0
+			continue
+		}
+		size := common.MustAtoi(args[0])
+		if shouldAdd {
+			for d, _ := range stack {
+				p := path(stack[:d+1])
+				if _, ok := dirs[p]; !ok {
+					dirs[p] = 0
+				}
+				dirs[p] = dirs[p] + size
+			}
+		}
+	}
+	sum := 0
+	for _, v := range dirs {
+		if v < 100000 {
+			sum += v
+		}
+	}
+	return sum
+}
+
+func path(stack []string) string {
+	path := ""
+	for _, s := range stack {
+		path += "/" + s
+	}
+	return path
 }
 
 func run2(input string) interface{} {
-	//list := common.ParseIntList(input, "\n")
-	return 0
+	list := common.ParseStringList(input, "\n")
+	var stack []string
+	dirs := map[string]int{}
+	shouldAdd := true
+	for _, row := range list {
+		if row == "$ cd .." {
+			stack = stack[:len(stack)-1]
+			continue
+		}
+		args := strings.Split(row, " ")
+		if args[0] == "dir" || args[1] == "ls" {
+			continue
+		}
+		if args[1] == "cd" {
+			stack = append(stack, args[2])
+			dirs[path(stack)] = 0
+			continue
+		}
+		size := common.MustAtoi(args[0])
+		if shouldAdd {
+			for d, _ := range stack {
+				p := path(stack[:d+1])
+				if _, ok := dirs[p]; !ok {
+					dirs[p] = 0
+				}
+				dirs[p] = dirs[p] + size
+			}
+		}
+	}
+	unused := 70000000 - dirs["//"]
+	required := 30000000 - unused
+	smallest := 70000000
+	for _, v := range dirs {
+		if v > required && v < smallest {
+			smallest = v
+		}
+	}
+	return smallest
 }
 
 func main() {
-	submit := true
+	submit := false
 	verbose := false
 	common.Run(run1, run2, tests, 2022, 7, submit, verbose)
 }
